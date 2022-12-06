e719bc6b16c18b386efe80dfede297324d912e75
gauravtamhan <gaurav.tamhan@gmail.com>
2022-12-06 17:59:54 -0500

    Completed Day 6

---
+++ 2022/06/part2.js
@@ -0,0 +1,19 @@
+import { readFile } from '../../utils.js'
+
+const MARKER_SIZE = 14
+
+const lines = readFile()
+const arr = lines[0].split('')
+
+for (let i = MARKER_SIZE - 1; i < arr.length; i++) {
+  const m = []
+  for (let j = 0; j <= MARKER_SIZE - 1; j++) {
+    m.push(arr[i - j])
+  }
+  const mSet = new Set(m)
+
+  if (mSet.size === MARKER_SIZE) {
+    console.log(i + 1)
+    break
+  }
+}
